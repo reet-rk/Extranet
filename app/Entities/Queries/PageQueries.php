2033f7f78e781ef0eee6ce024ccb4c136bef6491
reet-rk <rrkanwale@gmail.com>
2025-06-14 15:11:18 +0530

    Query Changes

--- app/Entities/Queries/PageQueries.php
+++ app/Entities/Queries/PageQueries.php
@@ -15,7 +15,7 @@ class PageQueries implements ProvidesEntityQueries
     ];
     protected static array $listAttributes = [
         'name', 'id', 'slug', 'book_id', 'chapter_id', 'draft',
-        'template', 'text', 'created_at', 'updated_at', 'priority', 'owned_by',
+        'template', 'text', 'created_at', 'updated_at', 'priority', 'updated_by', 'owned_by',
     ];
 
     public function start(): Builder
